58430ce9cb2c6d942c92215de690ad3e9252d45a
Robin <44874951+r-grun@users.noreply.github.com>
2023-02-28 12:01:58 +0100

    Add mongo docker

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,12 +1,21 @@
-db.createUser(
-    {
-        user: "gym_user",
-        pwd: "gym",
-        roles: [
-            {
-                role: "readWrite",
-                db: "gym"
-            }
-        ]
-    }
-);
+db.createUser({
+    user: process.env.MONGO_GYM_USERNAME,
+    pwd: process.env.MONGO_GYM_PASSWORD,
+    roles: [
+        {
+            role: "readWrite",
+            db: "gymDB",
+        },
+    ],
+});
+
+db = new Mongo().getDB("gymDB");
+
+db.createCollection('checkin', {
+    timeseries: {
+        timeField: 'timestamp'
+    },
+    capped: false
+});
+
+db.checkin.insert([{'timestamp': '2023-02-28T11:18:08.845Z', 'centerId': '320', 'webName': 'Hamburg-Wandsbek', 'status': 1, 'currentlyCheckedInCount': '60', 'maximumAllowedCheckedIn': '438'}]);
