2488528a2525e4fc30a5d9ea3fd16d94569e6e52
Robin <44874951+r-grun@users.noreply.github.com>
2023-03-08 17:19:18 +0100

    Add homepage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { CapacityCurrentComponent } from './features/capacity/capacity-current/capacity-current.component';
 import { BookingsComponent } from './features/bookings/bookings.component';
 import { HomeComponent } from './core/components/home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CapacityCurrentComponent, BookingsComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CapacityCurrentComponent,
+    BookingsComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
