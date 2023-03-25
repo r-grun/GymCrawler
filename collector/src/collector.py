703fd65b30a7b80467306dcce0ea5af972743a54
Robin <44874951+r-grun@users.noreply.github.com>
2023-03-25 17:11:39 +0100

    Update collector script

--- collector/src/collector.py
+++ collector/src/collector.py
@@ -4,7 +4,6 @@ import time
 import logging
 import pymongo
 from datetime import datetime
-from pytz import timezone
 import traceback
 import os
 
@@ -50,7 +49,7 @@ try:
                 gym_data = json.loads(response.text)
                 selected_gym_data = gym_data['capacityInfos']['centerId' == gym_id] 
 
-                timestamp = datetime.now(timezone('Europe/Berlin')).isoformat()
+                timestamp = datetime.today().replace(microsecond=0)
                 selected_gym_data['timestamp'] = timestamp
 
                 logging.info(f'Saving to db: {selected_gym_data}')
